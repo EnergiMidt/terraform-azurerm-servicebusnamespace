09467569c203fcdb9f78e7d68e895690312a1ed6
Leonard Sheng Sheng Lee <leonard.shengshenglee@tensio.no>
2023-06-29 13:36:17 +0200

    fix: typographical error

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "azurerm_servicebus_namespace" {
-  description = "The created Azure Service Bus namespace."
+  description = "The created Azure Service Bus Namespace."
   value       = azurerm_servicebus_namespace.servicebus_namespace
 }
